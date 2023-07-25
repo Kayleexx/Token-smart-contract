ee99ee32e81bf45e4d1c6a23427269441c3068d5
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2023-07-25 17:57:33 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-
-  console.log("Deploying contracts with the account:", deployer.address);
+   console.log("Deploying contracts with the account:", deployer.address);
 
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
