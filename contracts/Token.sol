dea7dbfa3a68ed672f51825f187d5a6e59bae270
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2023-07-28 22:22:53 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,26 +1,33 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT 
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-    address private _owner;
+contract Token is ERC20 { address private _owner;
 
-    constructor() ERC20("MyToken", "MTK") {
-        _owner = msg.sender;
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Only owner can perform this action");
-        _;
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
+constructor() ERC20("KayleesToken", "KAL") {
+    _owner = msg.sender;
+    _mint(msg.sender, 1000000 * 10 ** decimals());
 }
+
+modifier onlyOwner() {
+    require(msg.sender == _owner, "Only owner can perform this action");
+    _;
+}
+
+function mint(address to, uint256 amount) public onlyOwner {
+    _mint(to, amount);
+}
+
+function burn(uint256 amount) public {
+    _burn(msg.sender, amount);
+}
+
+function transfer(address recipient, uint256 amount) public override returns (bool) {
+    _transfer(msg.sender, recipient, amount);
+    return true;
+}
+
+}
+
